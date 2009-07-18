5fd1acb540042b69f0c64c5b9c4f875582681ee5
Kang-min Liu <gugod@gugod.org>
2009-07-19 00:18:13 +0800

    Checking in changes prior to tagging of version 0.12.  Changelog diff is:

--- lib/Finance/Bank/SCSB/TW.pm
+++ lib/Finance/Bank/SCSB/TW.pm
@@ -4,7 +4,7 @@ package Finance::Bank::SCSB::TW;
 
 use Carp;
 use 5.008;
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 use WWW::Mechanize;
 use HTML::Selector::XPath qw(selector_to_xpath);
 use HTML::TreeBuilder::XPath;
